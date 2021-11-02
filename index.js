630d6cdef29c4f62593639f7750fd99463baedac
w-hosinski <woj.hosinski@gmail.com>
2021-11-02 15:36:27 +0100

    Button für API Title request
    Button hinzugefügt, welcher einen Zufälligen Titel von der jsonplaceholder.typicode.com/todos API requested und diesen in das Title feld einfügt

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 import AddBook from "./AddBook.js"
 import DisplayBookList from "./DisplayBookList.js"
 
-document.getElementById("submitNewBook").addEventListener("click", AddBook)
 window.bookList = []
+
+const getNewBook = () => {
+const request = new XMLHttpRequest() 
+let randomNum = Math.ceil(Math.random() * 200)
+
+request.addEventListener("readystatechange", () => {
+    if(request.readyState === 4 && request.status === 200) {
+        let answer = request.responseText
+        submitNewBookTitle.value = JSON.parse(answer).title
+        console.log(request.responseText)
+    }
+    else if(request.readyState === 4) console.log("error when fetching the data")
+})
+
+request.open("GET", `https://jsonplaceholder.typicode.com/todos/${randomNum}`)
+request.send()
+}
+
+document.getElementById("submitNewBook").addEventListener("click", AddBook)
+document.getElementById("getRandomBook").addEventListener("click", getNewBook)
